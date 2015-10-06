4cb44f4474c2e1b5bcc959285a3ba788a56a9785
MichaelFHScott <mhs@iqx.co.uk>
2015-10-06 16:35:06 +0100

    Created zip.bat to zip up the production files (requires 7zip in the path)

---
+++ zip.bat
@@ -0,0 +1,3 @@
+7z a IQXWeb.zip server.js install.bat iqxWeb.bat package.json serviceInstall.js views jobs
+7z a IQXWeb.zip assets -x!assets\custom -x!assets\maps
+7z a IQXWeb.zip srvControllers  -x!srvControllers\custom
