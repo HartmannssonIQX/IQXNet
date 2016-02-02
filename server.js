f496518fc16f65f4b0fb49e86d578e52779b4dcf
MichaelFHScott <mhs@iqx.co.uk>
2016-02-02 12:33:45 +0000

    Add username to logging Add optional daily file logging instead of console

--- server.js
+++ server.js
@@ -6,9 +6,34 @@ var http = require('http')
 var fs = require('fs')
 var config=require('./config')
 var app = express()
+var FileStreamRotator = require('file-stream-rotator')
+var moment = require('moment')
 
-app.use(bodyParser.json())
-app.use(logger('dev'))
+//Set up the routing middleware:
+app.use(bodyParser.json())  // Parse JSON request body into req.body
+
+app.use(require('./srvControllers/auth'))  // Decode the x-auth header. This is required here so as to display the username in the log
+
+var logFormat=':method :url | :req[iqxauthuser] | :status | :response-time ms | :res[content-length] bytes'
+if (config.logToFile) {
+  var logDir=__dirname+'/logs'
+  try {
+    fs.mkdirSync(logDir)
+  } catch(err) {}
+  logger.token('time', function(){ return moment().format('HH:mm:ss') })
+  logFormat=':time '+logFormat
+  var dailyLogStream = FileStreamRotator.getStream({
+    filename: logDir + '/%DATE%.log',
+    frequency: 'daily',
+    verbose: false,
+    date_format: 'YYYY-MM-DD'
+    })
+  app.use(logger(logFormat,{stream: dailyLogStream}))
+} else {  // Just show on console
+  app.use(logger(logFormat))
+}
+
+// Route the requests to the controllers:
 app.use(require('./srvControllers'))
 
 var port=process.env.PORT || config.publicPort   // A separate server instance for automated testing can be ensured by setting env.PORT in the test harness
--- srvControllers/index.js
+++ srvControllers/index.js
@@ -1,7 +1,5 @@
 var router = require('express').Router()
 
-//router.use(require('./auth'))  // set req.auth to decoded contents of req.headers['x-auth'] // Not currently required
-
 router.use('/api/maint', require('./apiMaint'))
 router.use('/api/document', require('./apiDocument'))
 router.use('/api/auth', require('./apiAuth'))
