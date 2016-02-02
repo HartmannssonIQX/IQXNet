e762c46cc79526f8ead717988272de897714d748
Gareth Johnstone <gareth.johnstone@iqx.co.uk>
2016-02-02 15:53:56 +0000

    Removing Files

--- test/col/coltest.js
+++
@@ -1,7 +0,0 @@
-var expect = require('chai').expect
-describe('Misc', function(){
-    it('Should be rational', function(){
-      expect(1).to.equal(1);
-      expect(2).not.to.equal(1);
-    })
-})
